6660cef496e543afb62c2f08ca9c0985a778f027
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-30 11:42:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -0,0 +1,16 @@
+package com.example.inputpengguna
+
+import android.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+
+
